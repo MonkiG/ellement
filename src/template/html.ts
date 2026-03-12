36299b54a7af189f6e8ff43dc79e44120e012fdf
MonkiG <raan.heam@gmail.com>
2026-03-12 16:17:49 -0600

    feat: make html parser and add utilities for css and type handling

--- src/template/html.ts
+++ src/template/html.ts
@@ -10,3 +10,42 @@ export function html(
     type: "html",
   };
 }
+
+export function htmlParser(html: TemplateResult, css?: TemplateResult): string {
+  let htmlString = "";
+  let cssString = "";
+
+  html.strings.forEach((str, i) => {
+    htmlString += str;
+    if (i < html.values.length) {
+      htmlString += normalizeValue(html.values[i]);
+    }
+  });
+
+  if (css) {
+    css.strings.forEach((str, i) => {
+      cssString += str;
+      if (i < css.values.length) {
+        cssString += normalizeValue(css.values[i]);
+      }
+    });
+  }
+
+  return (cssString ? `<style>${cssString}</style>` : "") + htmlString;
+}
+
+
+export function isRenderObject(
+  result: TemplateResult | {
+    styles?: TemplateResult;
+    html: TemplateResult;
+  }
+): result is { styles?: TemplateResult; html: TemplateResult } {
+  return typeof result === "object" && "html" in result;
+}
+
+function normalizeValue(value: unknown): string {
+  if(value == null || value === false) return ""
+  if(Array.isArray(value)) return value.map(normalizeValue).join("");
+  return String(value)
+}
